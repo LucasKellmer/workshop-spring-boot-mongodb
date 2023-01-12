b88f90b5297ea034752e80c9b779250898956a77
Lucas Kellmer <lucaskellmer@hotmail.com>
2023-01-12 10:31:56 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/lucaskellmer/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.lucaskellmer.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucaskellmer.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
